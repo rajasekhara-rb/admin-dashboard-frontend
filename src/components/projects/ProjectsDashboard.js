aa09c9c51028e3aa6fc4d6c1385723ca4f95dd56
Budda Rajasekhara Reddy <rajasekharabudda009@gmail.com>
2023-09-11 21:14:53 +0530

    updated

--- src/components/projects/ProjectsDashboard.js
+++ src/components/projects/ProjectsDashboard.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Card, CardBody, CardHeader, Input } from "reactstrap";
+import { Button, Card, CardBody, CardHeader, FormGroup, Input, Label } from "reactstrap";
+import PaymentsTable from "./paymentsTable";
+import SalesChart from "../charts/salesChart";
 
 const ProjectDashboard = ({ baseUrl }) => {
     const token = localStorage.getItem("jwt");
@@ -71,7 +73,7 @@ const ProjectDashboard = ({ baseUrl }) => {
                         }}
                 >
                     {
-                        projects?.map((p) => {
+                        [projects]?.map((p) => {
                             return (
                                 <option value={p._id} >
                                     {p.projectName}
@@ -82,10 +84,64 @@ const ProjectDashboard = ({ baseUrl }) => {
 
                 </Input>
                 <CardHeader>
-                    Project Name
+                    {projectById.projectName}NAMW
                 </CardHeader>
                 <CardBody>
+                    <Card style={{ height: "100%" }}>
+                        <CardHeader >
+                            <FormGroup style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+                                <Label for="exampleSelect" style={{ width: "20%" }}>
+                                    Sales
+                                </Label>
+                                <div style={{ width: "20%", display: "flex" }}>
+                                    <Input
+                                        id="exampleSelect"
+                                        name="select"
+                                        type="select"
+                                        style={{ width: "100%", padding: "10px", marginRight: "10px" }}
+                                        // onChange={(e) => {
+                                        //     // setSelectedSales(e.target.value);
+                                        //     setSelectedSales(e.target.value);
+                                        //     getSalesByMonth()
+                                        // }}
+                                        onChange={
+                                            (e) => {
+                                                // handleSalesChartChange(e);
+                                                // getSalesByMonth()
+                                            }}
+                                    >
+                                        <option value="daily" selected>
+                                            Daily
+                                        </option >
+                                        <option value="monthly">
+                                            Monthly
+                                        </option>
+                                        <option value="yearly">
+                                            Yearly
+                                        </option>
+                                    </Input>
+                                    <Button
 
+                                    // onClick={() => { getSales() }}
+                                    >
+                                        Go</Button>
+                                </div>
+                            </FormGroup>
+                        </CardHeader>
+                        <CardBody style={{ margin: "auto", width: "100%" }}>
+                            {/* {
+                                selectedSales ? (
+                                    <SalesChart labels={salesLables} amounts={salesAmount} />
+                                ) : (
+                                    "Loading..."
+                                )
+                            } */}
+                            <SalesChart />
+                        </CardBody>
+                    </Card>
+                </CardBody>
+                <CardBody>
+                    <PaymentsTable />
                 </CardBody>
             </Card>
         </>
